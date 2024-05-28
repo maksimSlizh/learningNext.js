3225655e433c5a1884b166b718c8f5fa84177af5
@Maival1 <maival.cimba@gmail.com>
2024-05-28 21:24:54 +0200

    feat(about, layout, error): creating about page, layout and handling page loading error

---
+++ next01/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function About() {
+  // throw new Error('Not found')
+  return (
+    <>
+      <h1>About</h1>
+      <Link href="/">Home</Link>
+    </>
+  )
+}
